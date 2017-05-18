1149d2c09366ed072eeb0c0adcf051f6b9988cc5
김정환 <ej88ej@gmail.com>
2017-05-18 21:55:02 +0900

    리팩토링

--- src/test/java/com/woowahan/web/HomeControllerTest.java
+++ src/test/java/com/woowahan/web/HomeControllerTest.java
@@ -1,29 +1,17 @@
 package com.woowahan.web;
 
+import com.woowahan.support.AbstractAcceptanceTest;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
-/**
- * Created by woowahan on 2017. 5. 18..
- */
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class HomeControllerTest {
+public class HomeControllerTest extends AbstractAcceptanceTest {
     private static final Logger log = LoggerFactory.getLogger(HomeControllerTest.class);
-
-    @Autowired
-    TestRestTemplate template;
 
     @Test
     public void home() throws Exception {
--- src/test/java/com/woowahan/web/UserControllerTest.java
+++ src/test/java/com/woowahan/web/UserControllerTest.java
@@ -1,29 +1,19 @@
 package com.woowahan.web;
 
+import com.woowahan.support.AbstractAcceptanceTest;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.*;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
-
 import java.util.Arrays;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class UserControllerTest {
+public class UserControllerTest extends AbstractAcceptanceTest {
     private static final Logger log = LoggerFactory.getLogger(HomeControllerTest.class);
-
-    @Autowired
-    TestRestTemplate template;
 
     @Test
     public void create() throws Exception {
